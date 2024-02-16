05d168955912c3e4cea4ad8334d95c86559e0928
napaintga <123561665+napaintga@users.noreply.github.com>
2024-02-16 18:23:21 +0200

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,52 +1,93 @@
-from typing import Any 
-from pygame import* 
-from random import randint 
-racket = 0 
-tenis_ball = 0 
-class GameSprite(sprite.Sprite):  
+from pygame import *
+'''Необхідні класи'''
+ 
+# клас-батько для спрайтів
+class GameSprite(sprite.Sprite):
+    #конструктор класу
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        # кожен спрайт повинен зберігати властивість image - зображення
+        self.image = transform.scale(image.load(player_image), (wight, height)) #разом 55,55 - параметри
+        self.speed = player_speed
+        # кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+# клас-спадкоємець для спрайту-гравця (керується стрілками)    
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+ 
+#ігрова сцена:
+back = (200, 255, 255)  #колір фону (background)
+win_width = 600
+win_height = 500
+
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+ 
+#прапорці, що відповідають за стан гри
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+ 
+#створення м'яча та ракетки  
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+ 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 56, 23))
+ 
+speed_x = 3
+speed_y = 3
+ 
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
   
-    def __init__(self, player_image , player_x , player_y, size_x, size_y, player_speed):  
-        sprite.Sprite.__init__(self)  
-        self.image = transform.scale(image.load(player_image),(50 , 50))   
-        self.speed = player_speed  
-        self.rect = self.image.get_rect()  
-        self.rect.x = player_x  
-        self.rect.y = player_y 
-     
- 
-class Player(GameSprite):  
-    def update(self):  
-        #key = K_SPACE()  
-        keys_pressed = key.get_pressed() 
-        if keys_pressed [K_UP] and self.rect.x > 5 :  
-            self.rect.x -= self.speed    
- 
-            keys_pressed = key.get_pressed()  
-        if keys_pressed [K_DOWN] and self.rect.x < win_width - 50 :  
-            self.rect.x += self.speed 
-  
-class Player(GameSprite):  
-    def update(self):  
-        #key = K_SPACE()  
-        keys_pressed = key.get_pressed() 
-        if keys_pressed [K_w] and self.rect.x > 5 :  
-            self.rect.x -= self.speed    
- 
-            keys_pressed = key.get_pressed()  
-        if keys_pressed [K_s] and self.rect.x < win_width - 50 :  
-            self.rect.x += self.speed  
-win_width = 700 
-win_height = 500 
- 
-racket_1 = 'racket.png' 
-racket_2 = 'racket.png' 
-tenis_ball = 'tenis_ball.png' 
- 
-racket_1 = Player(racket, 10, win_height - 100, 80, 100, 20) 
-racket_2 = Player(racket, 10, win_height - 100, 80, 100, 20)  
-tenis_ball = GameSprite() 
- 
-window = display.set_mode((win_width, win_height))  
-display.set_caption("Shooter Game") 
- 
-display = display.update()
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        #якщо м'яч досягає меж екрана, змінюємо напрямок його руху
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        #якщо м'яч відлетів далі ракетки, виводимо умову програшу для першого гравця
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+        #якщо м'яч полетів далі ракетки, виводимо умову програшу другого гравця
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
